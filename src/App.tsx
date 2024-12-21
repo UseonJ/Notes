6bbd329ad55d4d805b2414d5b5c19bf047b1e978
UseonJ <naul630@gmail.com>
2024-12-21 23:23:26 +0900

    init: project init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="name">
+      <h1>Aron</h1>
+      </div>
+      <div className="title">
+        <p>
+          Developing Notes.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
