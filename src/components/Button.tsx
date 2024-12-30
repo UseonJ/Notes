fb1b3589c8b3b1fa3411ed45e0f6100d7b76bb16
UseonJ <naul630@gmail.com>
2024-12-30 23:47:57 +0900

    feat: button 컴포넌트

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Button.css';
+
+interface ButtonProps {
+    label: string;
+    onClick: () => void;
+}
+
+export const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
+    return <button className="btn" onClick={onClick}>{label}</button>;
+};
