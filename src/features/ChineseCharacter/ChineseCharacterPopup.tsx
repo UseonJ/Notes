f2e94f168164ea4a24fbd1c2e94e6be7426092cc
Aron0824 <useonjeong824@gmail.com>
2024-12-30 23:48:39 +0900

    init: chinese character popup

---
+++ src/features/ChineseCharacter/ChineseCharacterPopup.tsx
@@ -0,0 +1,7 @@
+import { Text } from "../../components/Text"
+
+export const ChineseCharacterPopup = () => {
+    return <div className="main_page_container">
+        <Text content="길모퉁이 우, 참선할 선" />
+    </div>
+}
