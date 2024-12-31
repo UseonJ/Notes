427a530594af4db846941b748a1296a8e470bc12
UseonJ <naul630@gmail.com>
2024-12-31 23:42:57 +0900

    fix: components 기본속성 변경

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -2,7 +2,7 @@ interface DividerProps {
     visible?: boolean;
 }
 
-export const Divider: React.FC<DividerProps> = ({ visible = false }) => {
+export const Divider: React.FC<DividerProps> = ({ visible = true }) => {
     return (
         <div className={visible ? "border-t border-b border-gray-300 my-4" : "hidden"}>
         </div>
