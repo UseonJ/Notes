9d26562f57482ae598cc75afce40c5d23a04c6f3
Aron0824 <useonjeong824@gmail.com>
2024-12-31 23:43:25 +0900

    fix: main page 구조 변경

--- src/features/Main/MainPage.tsx
+++ src/features/Main/MainPage.tsx
@@ -1,15 +1,7 @@
-import { Text } from "../../components/Text"
-import { Divider } from "../../components/Divider"
-import { Button } from "../../components/Button"
-
-const moveToChineseCharacter = () => {
-}
+import { Card } from "../../components/Card"
 
 export const MainPage = () => {
     return <div className="main_page_container">
-        <Text content="Name : Aron, 정우선" />
-        <Button label="한자 뜻 풀이" onClick={moveToChineseCharacter}/>
-        <Divider />
-        <Text content="" />
+        <Card leftText="Name" rightText="Aron" />
     </div>
 }
