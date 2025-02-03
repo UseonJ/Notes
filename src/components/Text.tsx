10273362111f7c2c5fbd0d6b02bdeb33a65cf78b
UseonJ <naul630@gmail.com>
2025-02-03 14:30:46 +0900

    remove: 제대로된 FSD 등 적용을 위한 정리

--- src/components/Text.tsx
+++
@@ -1,8 +0,0 @@
-interface TextProps {
-    content: string;
-    color?: string;
-}
-
-export const Text: React.FC<TextProps> = ({ content, color = "black" }) => {
-    return <div className={`text-${color}`}>{content}</div>
-}
--- src/features/Main/MainPage.tsx
+++
@@ -1,7 +0,0 @@
-import { Card } from "../../components/Card"
-
-export const MainPage = () => {
-    return <div className="main_page_container">
-        <Card leftText="Name" rightText="Aron" leftColor="orange-500" rightColor="red-600"/>
-    </div>
-}
