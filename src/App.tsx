977341d10353db59fa6c08e78d1c8dc6f90a8bde
UseonJ <naul630@gmail.com>
2025-02-12 16:22:22 +0900

    fix: 본격 개발 전 소규모 변경사항 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,13 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className="app_container">
-      <div className='name_eng_container p-4 m-4'>
-      <p className='name_eng text-black'>Aron</p>
-      </div>
-      <div className='name_kr_container p-4 m-4'>
-        <p className='name_kor text-white'>정우선</p>
-      </div>
-      <div className='word_explanation_container p-4 m-4'>
-        <p>개발자</p>
-      </div>  
+      <div className="app_container flex flex-row items-center justify-center">
+        <div className='nickname_container p-4 m-4'>
+          <p className='nickname text-black'>Aron</p>
+        </div>
+        <div className='content_container p-4 m-4'>
+          <p className='content_kor text-white'>진화하는 개발 노트</p>
+        </div> 
       </div>
     </>
   )
