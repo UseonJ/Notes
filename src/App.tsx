b494bac307eba342acaecbda222cd01155b5d9ea
UseonJ <naul630@gmail.com>
2025-02-13 23:37:34 +0900

    feat: main_page setting

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className="app_container flex flex-row items-center justify-center">
-        <div className='nickname_container p-4 m-4'>
-          <p className='nickname text-black'>Aron</p>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center gap-x-4">
+        <div className="shrink-0">
+          <img className="main_cover_photo" src="/main_cover.jpg" alt="Main Page Cover Picture Aron is Standing"/>
         </div>
-        <div className='content_container p-4 m-4'>
-          <p className='content_kor text-white'>진화하는 개발 노트</p>
+      </div>
+      <div className="app_container flex flex-row items-center justify-center">
+        <div className='name_container flex flex-col items-center justify-center'>
+          <div className='nickname_container'>
+            <p className='nickname text-black font-yesmyoungjo'>Aron</p>
+          </div>
+          <div className='content_container'>
+            <p className='content text-white font-yesmyoungjo'>Developing Portfolio</p>
+          </div>
         </div> 
       </div>
     </>
